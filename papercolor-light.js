c822f9a4fed90db00dcf075c80e00fe7ad952042
Bobby Welch <bobby@welch.xyz>
2021-07-17 19:01:53 -0400

    Update papercolor theme

--- papercolor-light.js
+++ papercolor-light.js
@@ -1,23 +1,23 @@
 // https://github.com/arcticicestudio/nord-vim/issues/48
 // https://github.com/arcticicestudio/nord-xresources
 term_.prefs_.set("color-palette-overrides", [
-  "#f3f3f3",
-  "#8959a8",
-  "#718c00",
-  "#4271ae",
+  "#eeeeee",
+  "#af0000",
+  "#008700",
+  "#5f8700",
+  "#0087af",
+  "#878787",
   "#005f87",
-  "#d7005f",
-  "#3e999f",
-  "#d0d0d0",
-  "#808080",
-  "#8959a8",
-  "#718c00",
-  "#4271ae",
+  "#444444",
+  "#bcbcbc",
+  "#d70000",
+  "#d70087",
+  "#8700af",
+  "#d75f00",
+  "#d75f00",
+  "#005faf",
   "#005f87",
-  "#d7005f",
-  "#3e999f",
-  "#262626",
 ]);
-term_.prefs_.set("foreground-color", "4d4d4c");
-term_.prefs_.set("background-color", "f3f3f3");
+term_.prefs_.set("foreground-color", "d0d0d0");
+term_.prefs_.set("background-color", "eeeeee");
 term_.prefs_.set("cursor-color", "rgba(188, 188, 188, 0.5)");
